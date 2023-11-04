f4f3b5a9e6004a68dd39e66569e68e65c4e417bc
Samuel Omisakin <94851485+c21448272@users.noreply.github.com>
2023-11-04 19:04:16 +0000

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -13,108 +13,167 @@ function App() {
 
 function TodoListCard() {
     const [items, setItems] = React.useState(null);
-    const [completedItems, setCompletedItems] = React.useState([]);
-    const [incompletedItems, setIncompletedItems] = React.useState([]);
 
     React.useEffect(() => {
         fetch('/items')
             .then(r => r.json())
-            .then(data => {
-                setItems(data);
-                // Filter and set completed and incompleted items
-                setCompletedItems(data.filter(item => item.completed));
-                setIncompletedItems(data.filter(item => !item.completed));
-            });
+            .then(setItems);
     }, []);
 
     const onNewItem = React.useCallback(
         newItem => {
             setItems([...items, newItem]);
-            if (newItem.completed) {
-                setCompletedItems([...completedItems, newItem]);
-            } else {
-                setIncompletedItems([...incompletedItems, newItem]);
-            }
         },
-        [items, completedItems, incompletedItems]
+        [items],
     );
-    
+
     const onItemUpdate = React.useCallback(
-    (item) => {
-        const updatedItems = [...items];
-        const updatedCompletedItems = [...completedItems];
-        const updatedIncompletedItems = [...incompletedItems];
-        const index = items.findIndex((i) => i._id === item._id);
+        item => {
+            const index = items.findIndex(i => i.id === item.id);
+            setItems([
+                ...items.slice(0, index),
+                item,
+                ...items.slice(index + 1),
+            ]);
+        },
+        [items],
+    );
 
-        updatedItems[index] = item;
-
-        if (item.completed) {
-            updatedCompletedItems.push(item);
-            updatedIncompletedItems = updatedIncompletedItems.filter((i) => i._id !== item._id);
-        } else {
-            updatedIncompletedItems.push(item);
-            updatedCompletedItems = updatedCompletedItems.filter((i) => i._id !== item._id);
-        }
-
-        setItems(updatedItems);
-        setCompletedItems(updatedCompletedItems);
-        setIncompletedItems(updatedIncompletedItems);
-    },
-    [items, completedItems, incompletedItems]
-);
     const onItemRemoval = React.useCallback(
-    item => {
-        // Remove the item from the items list
-        setItems(items.filter(i => i.id !== item.id));
-
-        // Update completedItems and incompletedItems based on the deleted item
-        if (item.completed) {
-            setCompletedItems(completedItems.filter(i => i.id !== item.id));
-        } else {
-            setIncompletedItems(incompletedItems.filter(i => i.id !== item.id));
-        }
-    },
-    [items, completedItems, incompletedItems]
-);
-
+        item => {
+            const index = items.findIndex(i => i.id === item.id);
+            setItems([...items.slice(0, index), ...items.slice(index + 1)]);
+        },
+        [items],
+    );
 
     if (items === null) return 'Loading...';
 
     return (
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
-            {incompletedItems.length === 0 && completedItems.length === 0 && (
+            {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
             )}
-            {incompletedItems.length > 0 && (
-                <div>
-                    <h2>Incomplete Items</h2>
-                    {incompletedItems.map(item => (
-                        <ItemDisplay
-                            item={item}
-                            key={item.id}
-                            onItemUpdate={onItemUpdate}
-                            onItemRemoval={onItemRemoval}
-                        />
-                    )}
-                </div>
-            )}
-            {completedItems.length > 0 && (
-                <div>
-                    <h2>Completed Items</h2>
-                    {completedItems.map(item => (
-                        <ItemDisplay
-                            item={item}
-                            key={item.id}
-                            onItemUpdate={onItemUpdate}
-                            onItemRemoval={onItemRemoval}
-                        />
-                    )}
-                </div>
-            )}
+            {items.map(item => (
+                <ItemDisplay
+                    item={item}
+                    key={item.id}
+                    onItemUpdate={onItemUpdate}
+                    onItemRemoval={onItemRemoval}
+                />
+            ))}
         </React.Fragment>
     );
 }
 
+function AddItemForm({ onNewItem }) {
+    const { Form, InputGroup, Button } = ReactBootstrap;
+
+    const [newItem, setNewItem] = React.useState('');
+    const [submitting, setSubmitting] = React.useState(false);
+
+    const submitNewItem = e => {
+        e.preventDefault();
+        setSubmitting(true);
+        fetch('/items', {
+            method: 'POST',
+            body: JSON.stringify({ name: newItem }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then(r => r.json())
+            .then(item => {
+                onNewItem(item);
+                setSubmitting(false);
+                setNewItem('');
+            });
+    };
+
+    return (
+        <Form onSubmit={submitNewItem}>
+            <InputGroup className="mb-3">
+                <Form.Control
+                    value={newItem}
+                    onChange={e => setNewItem(e.target.value)}
+                    type="text"
+                    placeholder="New Item"
+                    aria-describedby="basic-addon1"
+                />
+                <InputGroup.Append>
+                    <Button
+                        type="submit"
+                        variant="success"
+                        disabled={!newItem.length}
+                        className={submitting ? 'disabled' : ''}
+                    >
+                        {submitting ? 'Adding...' : 'Add Item'}
+                    </Button>
+                </InputGroup.Append>
+            </InputGroup>
+        </Form>
+    );
+}
+
+function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
+    const { Container, Row, Col, Button } = ReactBootstrap;
+
+    const toggleCompletion = () => {
+        fetch(`/items/${item.id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: item.name,
+                completed: !item.completed,
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then(r => r.json())
+            .then(onItemUpdate);
+    };
+
+    const removeItem = () => {
+        fetch(`/items/${item.id}`, { method: 'DELETE' }).then(() =>
+            onItemRemoval(item),
+        );
+    };
+
+    return (
+        <Container fluid className={`item ${item.completed && 'completed'}`}>
+            <Row>
+                <Col xs={1} className="text-center">
+                    <Button
+                        className="toggles"
+                        size="sm"
+                        variant="link"
+                        onClick={toggleCompletion}
+                        aria-label={
+                            item.completed
+                                ? 'Mark item as incomplete'
+                                : 'Mark item as complete'
+                        }
+                    >
+                        <i
+                            className={`far ${
+                                item.completed ? 'fa-check-square' : 'fa-square'
+                            }`}
+                        />
+                    </Button>
+                </Col>
+                <Col xs={10} className="name">
+                    {item.name}
+                </Col>
+                <Col xs={1} className="text-center remove">
+                    <Button
+                        size="sm"
+                        variant="link"
+                        onClick={removeItem}
+                        aria-label="Remove Item"
+                    >
+                        <i className="fa fa-trash text-danger" />
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
