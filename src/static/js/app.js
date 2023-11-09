e4016a31b8921182498ead1c635d8dcc8ffae2d6
Samuel Omisakin <94851485+c21448272@users.noreply.github.com>
2023-11-09 05:05:01 +0000

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,24 +1,43 @@
 function App() {
     const { Container, Row, Col } = ReactBootstrap;
+    const [completedCount, setCompletedCount] = React.useState(0);
+    const [incompletedCount, setIncompletedCount] = React.useState(0);
+
     return (
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                    <TodoListCard />
+                    <TodoListCard
+                        setCompletedCount={setCompletedCount}
+                        setIncompletedCount={setIncompletedCount}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} className="text-center">
+                    <div>Completed: {completedCount}</div>
+                    <div>Incompleted: {incompletedCount}</div>
                 </Col>
             </Row>
         </Container>
     );
 }
 
-function TodoListCard() {
-    const [items, setItems] = React.useState(null);
-
+function TodoListCard({ setCompletedCount, setIncompletedCount }) {
+    const [items, setItems] = React.useState([]);
+    
     React.useEffect(() => {
         fetch('/items')
-            .then(r => r.json())
-            .then(setItems);
-    }, []);
+            .then((r) => r.json())
+            .then((data) => {
+                setItems(data);
+                // Calculate counts for completed and incompleted tasks
+                const completedTasks = data.filter((item) => item.completed);
+                const incompletedTasks = data.filter((item) => !item.completed);
+                setCompletedCount(completedTasks.length);
+                setIncompletedCount(incompletedTasks.length);
+            });
+    }, [setCompletedCount, setIncompletedCount]);
 
     const onNewItem = React.useCallback(
         newItem => {
